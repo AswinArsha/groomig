47cba1f02bbdf3f010a908a4d031b4e21fbb245f
Aswin <109562208+AswinArsha@users.noreply.github.com>
2025-05-01 11:10:45 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://exkscztgagvuuxjhvypk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4a3NjenRnYWd2dXV4amh2eXBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAzNTEzMjEsImV4cCI6MjA0NTkyNzMyMX0.2weCQdWxNLfdtxAllP1FAKxwCMZnVTwNqqaIX9skCtk';
+const supabaseUrl = '';
+const supabaseKey = '';
 export const supabase = createClient(supabaseUrl, supabaseKey)
